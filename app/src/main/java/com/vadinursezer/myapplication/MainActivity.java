3307ffa0a2198adbd7b7ccc24bbd05733b493f3d
vadinur <vadinur@gmail.com>
2020-01-13 22:07:53 +0300

    Toast mesaj eklendi.

--- app/src/main/java/com/vadinursezer/myapplication/MainActivity.java
+++ app/src/main/java/com/vadinursezer/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.vadinursezer.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,7 +26,8 @@ SharedPreferences sharedPreferences;
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         sharedPreferences = this.getSharedPreferences("com.vadinursezer.myapplication",Context.MODE_PRIVATE);
-       int storeAge = sharedPreferences.getInt("storeAge",0);
+        int storeAge = sharedPreferences.getInt("storeAge",0);
+
 
         if (storeAge==0){
             resultAge.setText("Your Age: ");
@@ -34,8 +39,26 @@ SharedPreferences sharedPreferences;
     public void save (View view){
         if (!numberAge.getText().toString().matches("")){
             int userAge=Integer.parseInt(numberAge.getText().toString());
-            resultAge.setText("Your Age: " + userAge);
-            sharedPreferences.edit().putInt("storeAge",userAge).apply();
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Save");
+            alert.setMessage("Are you sure?");
+            alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    resultAge.setText("Your Age: " + userAge);
+                    sharedPreferences.edit().putInt("storeAge",userAge).apply();
+                    Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+                    numberAge.setText("");
+                }
+            });
+            alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "Not Saved",Toast.LENGTH_LONG).show();
+                    numberAge.setText("");
+                }
+            });
+            alert.show();
         }
     }
     public void clear(View view){
